049efc6734c4c16c9a844f1c727a83b23aa8072e
Alejandro Santos <alejolp@gmail.com>
2015-11-18 15:41:37 +0100

    run linux64

--- run-linux64.sh
+++ run-linux64.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-env SEGFAULT_USE_ALTSTACK="1" SEGFAULT_SIGNALS="abrt segv" LD_PRELOAD="/lib/x86_64-linux-gnu/libSegFault.so" ./bin/dakaraserver $1
+RUTA=$(dirname $0)
+TIMENOW=$(date "+%Y%m%d-%H%M%S.%N")
+
+env SEGFAULT_USE_ALTSTACK="1" SEGFAULT_SIGNALS="abrt segv" LD_PRELOAD="/lib/x86_64-linux-gnu/libSegFault.so" $RUTA/build/dakaraserver $1 2>&1 | tee logs/server.$TIMENOW
 
