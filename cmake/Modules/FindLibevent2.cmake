9ebc979e178dc456496fbf96711d93b82c9083ba
Alejandro Santos <alejolp@gmail.com>
2015-11-23 10:53:15 +0100

    appveyor

--- cmake/Modules/FindLibevent2.cmake
+++ cmake/Modules/FindLibevent2.cmake
@@ -2,13 +2,24 @@
 # https://github.com/sipwise/sems/blob/master/cmake/FindLibevent2.cmake
 #
 
-FIND_PATH(LIBEVENT2_INCLUDE_DIR event2/event.h HINTS /usr/include/event2 )
+if(MSVC)
+	FIND_PATH(LIBEVENT2_INCLUDE_DIR event2/event.h)
 
-# -levent -levent_core -levent_extra
+	if(CMAKE_CL_64)
+		FIND_LIBRARY(LIBEVENT2_LIBRARIES NAMES libevent-x64-v120-mt-gd-2_1_4_0 event libevent )
+	else()
+		FIND_LIBRARY(LIBEVENT2_LIBRARIES NAMES libevent-x86-v120-mt-2_1_4_0 event libevent )
+	endif()
+else()
+	FIND_PATH(LIBEVENT2_INCLUDE_DIR event2/event.h HINTS /usr/include/event2 )
 
-FIND_LIBRARY(LIBEVENT2_LIBRARIES NAMES event libevent )
-FIND_LIBRARY(LIBEVENT2_CORE_LIBRARIES NAMES event_core libevent_core )
-FIND_LIBRARY(LIBEVENT2_EXTRA_LIBRARIES NAMES event_extra libevent_extra )
+	# -levent -levent_core -levent_extra
+
+	FIND_LIBRARY(LIBEVENT2_LIBRARIES NAMES event libevent )
+	FIND_LIBRARY(LIBEVENT2_CORE_LIBRARIES NAMES event_core libevent_core )
+	FIND_LIBRARY(LIBEVENT2_EXTRA_LIBRARIES NAMES event_extra libevent_extra )
+endif()
+
 
 IF(LIBEVENT2_INCLUDE_DIR AND LIBEVENT2_LIBRARIES)
 	SET(LIBEVENT2_FOUND TRUE)
